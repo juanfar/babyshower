468ccbf41aa76ffecbb74b33a84cd9586ea8ff40
unknown <usuario@GFI10970.gfi.es>
2020-02-03 13:05:22 +0100

    Se implementa estrategia de precarga quicklink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from '@shared/shared.module';
+import { QuicklinkModule } from 'ngx-quicklink'
+
 import { fakeBackendProvider } from '@core/mocks/fake-backend';
 
 @NgModule({
@@ -15,7 +17,8 @@ import { fakeBackendProvider } from '@core/mocks/fake-backend';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    HttpClientModule
+    HttpClientModule,
+    QuicklinkModule
   ],
   providers: [fakeBackendProvider],
   bootstrap: [AppComponent]

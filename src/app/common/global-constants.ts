a3090954115d1a3817279e384beddee2a70f1963
Juan Fajardo <juanfar.group@gmail.com>
2020-02-09 21:11:34 +0100

    se configura global constant y send order

---
+++ src/app/common/global-constants.ts
@@ -0,0 +1,3 @@
+export class GlobalConstants {
+  public static apiURL = 'https://us-central1-babyshower-3ae23.cloudfunctions.net/api';
+}
--- src/app/core/decorators/confirmable.decorator.ts
+++ src/app/core/decorators/confirmable.decorator.ts
@@ -1,15 +1,15 @@
 import Swal from 'sweetalert2';
 
 export function Confirmable() {
-  return function (target: Object, key: string | symbol, descriptor: PropertyDescriptor) {
+  return (target: object, key: string | symbol, descriptor: PropertyDescriptor) => {
     const original = descriptor.value;
-    descriptor.value = function () {
-      const message = msg(key);    
+    descriptor.value = function() {
+      const message = msg(key);
 
       return confirmacion(this, original, message);
-    }
+    };
     return descriptor;
-  }
+  };
 }
 
 function confirmacion(obj, original, message) {
@@ -20,19 +20,20 @@ function confirmacion(obj, original, message) {
     showCancelButton: true,
     confirmButtonText: message.confirmButtonText,
     cancelButtonText: message.cancelButtonText
-  }).then((result) => {
-    if (result.value) {
+  }).then((res) => {
+    if (res.value) {
       Swal.fire(
         message.subtitle,
         message.subtext,
         'success'
-      )
+      );
       const result = original.apply(obj);
       return result;
-    } else return null;
+    } else {
+      return null;
+    }
   });
 }
-
 
 function msg(key) {
   return {
@@ -64,5 +65,5 @@ function msg(key) {
       key === 'sendOrder' ? 'Tus Regalos has sido reservados.' :
       key === 'cancelOrder' ? 'Puedes seleccionar nuevos productos' :
       '',
-  }
+  };
 }
